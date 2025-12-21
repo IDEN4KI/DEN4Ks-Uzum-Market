7f7fd250dcb8c98b0f22c6004b0eaaef3451b396
Danil <danilplaksin08@gmail.com>
2025-12-21 22:09:50 +0500

    header complete 2 of 3

--- src/components/Header.js
+++ src/components/Header.js
@@ -25,13 +25,45 @@ export const Header = `
         </div>
       </section>
 
-      <section id="header-middle"></section>
+      <section id="header-middle">
+        <div id="header-middle-left">
+          <img src="public/images/Uzum-Market-Logo-Vector.svg-.png" alt="logo" id="logo">
+          <div id="midale">
+            <img src="public/icons/Screenshot_3.jpg" alt="ya xz" id="midale-icon">
+            <button id="catalog-btn">Каталог</button>
+          </div>
+        </div>
+
+        <div id="header-middle-center">
+          <input type="text" placeholder="Искать товары и категории">
+          <div id="search-div">
+            <img src="public/icons/free-icon-search-5636698.png" alt="ya xz" id="search-icon">
+          </div> 
+        </div>
+
+        <div id="header-middle-right">
+          <div id="login-btn">
+            <img id="icon" src="public/icons/free-icon-user-icon-4360835.png" alt="login">
+            <a href="#">Данил</a>
+          </div>
+          <div id="favorite-btn">
+            <img id="icon" src="public/icons/free-icon-favorite-13426236.png" alt="login">
+            <a href="#">Избранное</a>
+          </div>
+          <div id="cart-btn">
+            <img id="icon" src="public/icons/free-icon-shopping-bag-icon-17287783.png" alt="login">
+            <a href="#">Корзина</a>
+          </div>
+        </div>
+      </section>
 
       <section id="header-bottom"></section>
     </div> 
   </header>
 
     <style>
+
+
       *{
         margin: 0;
         padding: 0;
@@ -43,6 +75,7 @@ export const Header = `
         width: 100%;
         flex-direction: column;
         justify-content: center;
+        align-items: center;
       }
 
       #palka{
@@ -51,6 +84,7 @@ export const Header = `
 
       .wrapper{
         display: flex;
+        flex-direction: column;
         width: 70%;
         align-items: center;
         justify-content: center;
@@ -58,6 +92,23 @@ export const Header = `
 
       #map-tracker{
         width: 3%;
+      }
+
+      #search-icon{
+        width: 30% ;
+        cursor: pointer;
+      }
+
+      #search-div{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 15% ;
+        border: 1px solid var(--black-a20) ;
+        border-radius: 0 5px 5px 0;
+        border-left: none;
+        cursor: pointer;
+        background-color: #edeff2;
       }
 
       #arrow{
@@ -94,14 +145,26 @@ export const Header = `
 
       }
 
+      #icon{
+        width: 17%;
+      }
 
+      #catalog-btn{
+        padding: 10px 14px;
+        background: var(--uzum-violet-95);
+        border: none;
+        border-radius: 5px;
+        font-weight: 500;
+        font-size: 14px;
+        color: var(--violet-55);
+        cursor: pointer;
+      }
       
       header{
         display: flex;
         width: 100%;
         align-items: center;
         justify-content: center;
-        background-color: #edeff2;
       }
 
       #button{
@@ -139,6 +202,78 @@ export const Header = `
         width: 100%;
       }
 
+      #midale{
+        display: flex;
+        align-items: center;
+        border-radius: 5px;
+        padding: 3px;
+        background: var(--uzum-violet-95);
+        cursor: pointer;
+      }
+
+      #midale-icon{
+        margin-left: 10px ;
+      }
+
+      #header-middle{
+        display: flex;
+        align-items: center;
+        justify-content: space-around; 
+        width: 100%;
+        height: 80px;
+        background: #fff;
+      }
+
+      #header-middle-center{
+        flex: 1;
+        max-width: 500px;
+        min-width: 500px;
+        display: flex;
+        margin: 0 10px;
+      }
+
+      #header-middle-right{
+        display: flex;
+        min-width: 330px;
+        gap: 10px;
+      }
+
+      #header-middle-right a {
+        text-decoration: none;
+        color: #000;
+        font-weight: 450;
+      }
+
+      #header-middle-center input{
+        flex: 1;
+        padding: 12px;
+        border: 1px solid var(--black-a20);
+        border-radius: 5px 0 0 5px;
+        outline: none;
+        border-right: none;
+      }
+
+      #header-middle-left{
+        display: flex;
+        align-items: center;
+        gap: 16px;
+      }
+
+      #logo {
+        height: 32px;
+        margin-left: 40px;
+        cursor: pointer;
+      }
+
+      #search-btn{
+        padding: 0 16px;
+        border: 1px solid var(--black-a20);
+        border-left: none;
+        background: #fff;
+        border-radius: 0 5px 5px 0;
+        cursor: pointer;
+      }
+
       #header-top-left{
         display: flex;
         margin-left: 15px;
@@ -158,6 +293,30 @@ export const Header = `
         width: 100%;
         margin-right: 15px;
         justify-content: space-around;
+      }
+
+      #login-btn{
+        display: flex;
+        gap: 10px;
+        margin-left: 40px;
+        cursor: pointer;
+
+      }
+
+      #favorite-btn{
+        display: flex;
+        gap: 10px;
+        margin-right: 20px;
+        cursor: pointer;
+
+      }
+
+      #cart-btn{
+        display: flex;
+        gap: 10px;
+        margin-right: 25px;
+        cursor: pointer;
+
       }
     </style>
 `;
